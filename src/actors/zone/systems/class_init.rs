f9f7867b6fad8a28afd7be673371d39e57fd83b1
Phil Lehmkuhl <phil@lehmkuhl.org>
2024-09-22 14:33:47 +0200

    Updates class ability experiments

--- src/actors/zone/systems/class_init.rs
+++ src/actors/zone/systems/class_init.rs
@@ -76,128 +76,128 @@ pub fn setup_combat_style_rage(
         player.set_current_class_skills(
             ClassSkills {
                 class_hash: 0x81E0A735,
-                count: 15,
+                count: 4,
                 skills: vec![
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("fe323341-4292-4830-879a-6e1e3f957493").unwrap(),
-                        group: "All_ClassAbility_SitAndHeal".to_string(),
-                        field_4: 4,
+                        group: "Recuperate".to_string(),
+                        //field_4: 1,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("0cb80a4b-4533-4194-a03d-3302b84240c3").unwrap(),
-                        group: "Rage_Class_PrimeChampion_Rank1".to_string(),
-                        //field_4: 3,
+                        group: "PrimeChampionNew".to_string(),
+                        //field_4: 1,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("ba5fd7b8-4d46-47f9-90d9-18d9bb344e32").unwrap(),
                         group: "ClassAbilities_New_Warrior_Damage_RawPower".to_string(),
-                        field_4: 1,
+                        //field_4: 1,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str(   "00000000-1111-2222-3333-000000000003").unwrap(), //Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("ab1f1bb1-a866-4f34-9b04-c952b39ea252").unwrap(),
                         group: "ClassAbilities_New_Warrior_Damage_BloodStorm".to_string(),
-                        field_4: 1,
+                        //field_4: 1,
                         ..Default::default()
                     },
-                    ClassSkill {
+                    /*ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("b98a8dd5-d50d-42c0-87a9-54813b8656c5").unwrap(),
                         group: "ClassAbilities_New_Warrior_Damage_Rampage".to_string(),
-                        field_4: 15,
+                        //field_4: 15,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("62f1b418-b72a-4bbd-8827-4a4a568e6d57").unwrap(),
                         group: "ClassAbilities_New_Warrior_Damage_Bane".to_string(),
-                        field_4: 25,
+                        //field_4: 25,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("f805b67c-4259-4aad-a840-9b3aed9c8f0e").unwrap(),
                         group: "ClassAbilities_New_Warrior_Damage_Banish".to_string(),
-                        field_4: 35,
+                        //field_4: 35,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("c2a6a790-cc12-4bb0-a2fa-cc18c1b937c3").unwrap(),
                         group: "ClassAbilities_New_Warrior_Damage_SanguineCurse".to_string(),
-                        field_4: 55,
+                        //field_4: 55,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("d1f3e2b1-05cd-4914-af5f-3c362283bbbc").unwrap(),
                         group: "PrimeGuardianNew".to_string(),
-                        field_4: 5,
+                        //field_4: 5,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("015bf264-14fd-494a-b758-be5218ac159b").unwrap(),
                         group: "ClassAbilities_New_Warrior_Tank_Dash".to_string(),
-                        field_4: 5,
+                        //field_4: 5,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("598a3005-5a7b-4982-ae69-7ea8bd3b32b8").unwrap(),
                         group: "ClassAbilities_New_Warrior_Tank_Taunt".to_string(),
-                        field_4: 5,
+                        //field_4: 5,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("11d52e0b-d053-4fb5-b92d-f2466872fe34").unwrap(),
                         group: "ClassAbilities_New_Warrior_Tank_VitalitySlash".to_string(),
-                        field_4: 25,
+                        //field_4: 25,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("d3ea59f7-5101-428a-a25f-988fb7f4482d").unwrap(),
                         group: "ClassAbilities_New_Warrior_Tank_Impulse".to_string(),
-                        field_4: 35,
+                        //field_4: 35,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("b880c31c-4e07-4913-a36a-ebe99c564341").unwrap(),
                         group: "ClassAbilities_New_Warrior_Tank_EssenceTouch".to_string(),
-                        field_4: 45,
+                        //field_4: 45,
                         ..Default::default()
                     },
                     ClassSkill {
                         version: 0,
-                        field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
+                        //field_1: Uuid::parse_str("7daff75b-6078-419b-aa75-c06799b21bf8").unwrap(),
                         content_id: Uuid::parse_str("65ae9a4f-56e0-42c1-94a8-2cc548119816").unwrap(),
                         group: "ClassAbilities_New_Warrior_Tank_Presage".to_string(),
-                        field_4: 55,
+                        //field_4: 55,
                         ..Default::default()
-                    },
+                    },*/
                 ]
             }.to_bytes()
         );
