b97ff5fc40c6cae3e30193e1a3d8fde9e87d5903
Phil Lehmkuhl <phil@lehmkuhl.org>
2024-10-20 03:18:00 +0200

    Add library for managing config files

---
+++ lib/config/src/lib.rs
@@ -0,0 +1,76 @@
+// Copyright (C) 2024 AnotherlandServer
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+mod types;
+
+use config_crate::File;
+use once_cell::sync::Lazy;
+use glob::glob;
+
+pub use types::*;
+
+pub static CLUSTER_CONF: Lazy<ConfClusterConfig> = Lazy::new(|| {
+    type Config = ::config_crate::Config;
+    
+    let mut builder = Config::builder()
+        .add_source(
+            glob("conf/*.toml")
+                .unwrap()
+                .map(|path| File::from(path.unwrap()))
+                .collect::<Vec<_>>(),
+        );
+
+    if cfg!(unix) {
+        builder = builder.add_source(
+            glob("/etc/anotherland/login/*.toml")
+                .unwrap()
+                .map(|path| File::from(path.unwrap()))
+                .collect::<Vec<_>>(),
+        );
+    }
+        
+    builder
+        .build()
+        .unwrap()
+        .try_deserialize::<ConfClusterConfig>()
+        .expect("Failed to parse config")
+});
+
+pub static REALM_CONF: Lazy<ConfRealmMain> = Lazy::new(|| {
+    type Config = ::config_crate::Config;
+    
+    let mut builder = Config::builder()
+        .add_source(
+            glob("conf/*.toml")
+                .unwrap()
+                .map(|path| File::from(path.unwrap()))
+                .collect::<Vec<_>>(),
+        );
+
+    if cfg!(unix) {
+        builder = builder.add_source(
+            glob("/etc/anotherland/realm/*.toml")
+                .unwrap()
+                .map(|path| File::from(path.unwrap()))
+                .collect::<Vec<_>>(),
+        );
+    }
+        
+    builder
+        .build()
+        .unwrap()
+        .try_deserialize::<ConfRealmMain>()
+        .expect("Failed to parse config")
+});
