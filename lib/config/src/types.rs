23040f971f0812842eb5c5c2dd7cce45c243718e
Phil Lehmkuhl <phil@lehmkuhl.org>
2024-10-24 01:48:00 +0200

    Removes unneded config attributes

--- lib/config/src/types.rs
+++ lib/config/src/types.rs
@@ -17,47 +17,18 @@ use std::net::{SocketAddrV4, SocketAddr};
 
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default)]
 pub struct ConfLoginServer {
     pub one_time_password_duration: Option<u32>,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct ConfClusterConfig {
+    #[serde(default)]
     pub login: ConfLoginServer,
 }
 
 #[derive(Debug, Deserialize)]
 #[allow(unused)]
-pub struct ConfRealmServer {
-    pub id: u32,
-    pub name: String,
-    pub listen_address: SocketAddrV4,
-}
-
-#[derive(Debug, Deserialize)]
-#[allow(unused)]
-pub struct ConfFrontendServer {
-    pub listen_address: SocketAddrV4,
-}
-
-#[derive(Debug, Deserialize)]
-#[allow(unused)]
-pub struct ConfApiServer {
-    pub listen_address: SocketAddr,
-}
-
-#[derive(Debug, Deserialize)]
-#[allow(unused)]
-pub struct ConfMetricsServer {
-    pub listen_address: SocketAddr,
-}
-
-#[derive(Debug, Deserialize)]
-#[allow(unused)]
 pub struct ConfRealmMain {
-    pub realm: ConfRealmServer,
-    pub frontend: ConfFrontendServer,
-    pub api: ConfApiServer,
-    pub metrics: ConfMetricsServer,
 }
