98c9886ffabf0926b2733ee60a495972e8416a5b
Phil Lehmkuhl <phil@lehmkuhl.org>
2024-11-18 00:09:26 +0100

    Adds commandline arguments to cluster_server

--- services/cluster_server/src/main.rs
+++ services/cluster_server/src/main.rs
@@ -13,11 +13,32 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::time::Duration;
+use std::{net::SocketAddr, time::Duration};
 
+use clap::Parser;
+use once_cell::sync::Lazy;
 use raknet::RakNetListener;
+use reqwest::Url;
 use tokio::time::sleep;
 use log::debug;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(long, env = "SERVICE_CORE_API_URL", default_value = "http://127.0.0.1:8000")]
+    service_core_url: Url,
+
+    #[arg(long, env = "SERVICE_REALM_API_URL", default_value = "http://127.0.0.1:8001")]
+    service_realm_url: Url,
+
+    #[arg(long, env = "REALM_ZMQ_ADDR", default_value = "tcp://127.0.0.1:15001")]
+    realm_zmq_addr: String,
+
+    #[arg(long, env = "RAKNET_BIND_ADDR", default_value = "0.0.0.0:6113")]
+    raknet_bind_addr: SocketAddr,
+}
+
+static ARGS: Lazy<Cli> = Lazy::new(Cli::parse);
 
 #[toolkit::service_main]
 async fn main() {
