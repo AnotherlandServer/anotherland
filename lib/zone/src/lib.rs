4d20f181d52b7dc624523627bdee20588aa49869
Phil Lehmkuhl <phil@lehmkuhl.org>
2024-11-18 00:09:59 +0100

    Adds Zone lib stub

--- lib/zone/src/lib.rs
+++ lib/zone/src/lib.rs
@@ -1,14 +1,18 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// Copyright (C) 2024 AnotherlandServer
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct Zone {
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
