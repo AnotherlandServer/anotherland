083e9d8b8d047d636137df64f0471f9c9e874287
plehmkuhl <phil@lehmkuhl.org>
2024-12-10 00:49:48 +0100

    Implements registration of cluster_server with realm_manager_service

--- services/cluster_server/src/main.rs
+++ services/cluster_server/src/main.rs
@@ -16,11 +16,17 @@
 use std::{net::SocketAddr, time::Duration};
 
 use clap::Parser;
+use core_api::CoreApi;
+use error::ClusterFrontendResult;
 use once_cell::sync::Lazy;
 use raknet::RakNetListener;
+use realm_api::{proto::{NodeAddress, NodeType, RealmClient, RealmRequest}, RealmApi};
 use reqwest::Url;
 use tokio::time::sleep;
 use log::debug;
+use toolkit::print_banner;
+
+mod error;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -34,15 +40,29 @@ struct Cli {
     #[arg(long, env = "REALM_ZMQ_ADDR", default_value = "tcp://127.0.0.1:15001")]
     realm_zmq_addr: String,
 
-    #[arg(long, env = "RAKNET_BIND_ADDR", default_value = "0.0.0.0:6113")]
+    #[arg(long, env = "RAKNET_BIND_ADDR", default_value = "0.0.0.0:6114")]
     raknet_bind_addr: SocketAddr,
+
+    #[arg(long, env = "PUBLIC_ADDR")]
+    public_addr: SocketAddr,
 }
 
 static ARGS: Lazy<Cli> = Lazy::new(Cli::parse);
 
 #[toolkit::service_main]
-async fn main() {
-    let mut listener = RakNetListener::bind("0.0.0.0:6112").await.unwrap();
+async fn main() -> ClusterFrontendResult<()> {
+    print_banner();
+
+    let realm_api = RealmApi::new(ARGS.service_realm_url.clone());
+    let core_api = CoreApi::new(ARGS.service_core_url.clone());
+
+    let (realm_client, notifications) = RealmClient::connect(&ARGS.realm_zmq_addr).await
+        .expect("failed to connect to realm zmq server");
+
+    // Register cluster node
+    realm_client.send(RealmRequest::RegisterNode(NodeType::Cluster, NodeAddress::Public(ARGS.public_addr))).await?;
+
+    let mut listener = RakNetListener::bind(ARGS.raknet_bind_addr).await.unwrap();
     listener.listen(100).await;
 
     loop {
